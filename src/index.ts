44e5648cd634551cbb324fb06f85125e0d59772b
Tomasz Bawor <bawortomasz@gmail.com>
2026-01-07 20:54:20 +0100

    chore: add effect.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import { Effect } from "effect";
+
+const program = Effect.gen(function* () {
+	yield* Effect.logInfo("App started ...");
+
+	yield* Effect.logInfo("Hello from effect");
+});
+
+Effect.runSync(program);
