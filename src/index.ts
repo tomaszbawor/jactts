2593030e15d0fb5f6a91f3bcebef77e4624f1e06
Tomasz Bawor <bawortomasz@gmail.com>
2026-01-08 21:32:20 +0100

    chore: simple queue examples

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,50 @@
 import { BunContext, BunRuntime } from "@effect/platform-bun";
-import { Effect } from "effect";
+import { Effect, Queue, Schedule } from "effect";
+
+const backgroundProducerTask = (queue: Queue.Queue<string>) => {
+	var iterator = 0;
+
+	return Effect.repeat(
+		Effect.gen(function* () {
+			const msg = `Message ${iterator}`;
+
+			yield* queue.offer(msg);
+			yield* Effect.logInfo(`Dodawanie do kolejki ${iterator++}`);
+		}),
+		Schedule.spaced("100 millis"),
+	);
+};
+
+const backtroundConsumerTask = (queue: Queue.Queue<string>) => {
+	return Effect.gen(function* () {
+		while (true) {
+			const msg = yield* queue.take;
+			yield* Effect.logInfo("Just got message ", msg);
+			yield* Effect.sleep("70 millis");
+		}
+	});
+};
 
 const program = Effect.gen(function* () {
 	yield* Effect.logInfo("App started ...");
 	yield* Effect.logInfo("Initializing ...");
+
+	const queue = yield* Queue.bounded<string>(5);
+	//
+	yield* Effect.fork(backgroundProducerTask(queue));
+	yield* Effect.sleep("1 second");
+
+	yield* Effect.fork(backtroundConsumerTask(queue));
+
+	yield* Effect.sleep("10 seconds");
 });
 
 const programWithDeps = program.pipe(Effect.provide(BunContext.layer));
+
+// const produceIntoQueue = (queue: Queue) =>
+// 	Effect.gen(function* () {
+// 		yield* Effect.logInfo("Putting stuff on queue ");
+// 		yield* queue.offer("Tomek");
+// 	});
 
 BunRuntime.runMain(programWithDeps);
